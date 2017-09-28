19531bfa84cde8f1f64fcef023f5fa89b1dd4f76
Andrew <awjstarling@gmail.com>
2017-09-28 18:34:11 +0300

    Full project upload

---
+++ Assets/Scripts/GUI/ResetScrollView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YleSearch
+{
+	public class ResetScrollView : MonoBehaviour
+	{
+		[SerializeField]
+		private ScrollRect scroll = null;
+
+		public void ResetToTop(){
+			scroll.normalizedPosition = Vector2.one;
+		}
+	}
+}
