dfe71f3c17441ede6edefe41a596e86d30101fbe
Andrew <awjstarling@gmail.com>
2017-09-28 18:34:11 +0300

    Full project upload

---
+++ Assets/Scripts/GUI/EnableDisableGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YleSearch
+{
+	public class EnableDisableGameObject : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject target = null;
+
+		public void ToggleEnabled ()
+		{
+			if (target.activeInHierarchy) {
+				target.SetActive (false);
+			} else {
+				target.SetActive (true);
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/GUI/PanelController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YleSearch
+{
+	[RequireComponent (typeof(AudioSource))]
+	public class PanelController : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject[] panels = null;
+
+		private int currentPanelIndex;
+		private GameObject currentPanel;
+		private Animator animator;
+		private AudioSource audioSource;
+
+		private void Start ()
+		{
+			currentPanelIndex = 0;
+			currentPanel = panels [currentPanelIndex];
+			animator = currentPanel.GetComponent<Animator> ();
+			audioSource = GetComponent<AudioSource> ();
+		}
+
+		private void Update ()
+		{
+				//android back button & desktop 'esc' trigger panel closing
+				if (Input.GetKeyUp (KeyCode.Escape)) {
+					Close ();
+				}
+		}
+
+		public void Open ()
+		{
+			if (currentPanelIndex < panels.Length - 1) {
+				currentPanelIndex++;
+				IteratePanel ();
+			}
+			if (animator != null) {
+				audioSource.pitch = 1.1f;
+				audioSource.PlayDelayed(0.1f);
+				animator.SetBool ("isOpen", true);
+			}
+		}
+
+		public void Close ()
+		{
+			if (currentPanelIndex > 0) {
+				if (animator != null) {
+					audioSource.pitch = 1;
+					audioSource.Play ();
+					animator.SetBool ("isOpen", false);
+				}
+				currentPanelIndex--;
+				IteratePanel ();
+			}
+		}
+
+		private void IteratePanel ()
+		{
+			currentPanel = panels [currentPanelIndex];
+			animator = currentPanel.GetComponent<Animator> ();
+		}
+
+		public GameObject GetCurrentPanel(){
+			return currentPanel;
+		}
+	}
+}
